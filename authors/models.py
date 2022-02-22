f908e58d061ec5971029c997030d8cbeffe4ef40
Andrew <diver.vlz@gmail.com>
2022-02-22 23:08:38 +0300

    lesson_1

---
+++ authors/models.py
@@ -0,0 +1,13 @@
+from uuid import uuid4
+
+from django.db import models
+
+
+class Author(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid4)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name[0]}. ({self.birthday_year} г.р.)'
