3248a89f6f49a0e47ec6ff2f82bf8ae04a0fa30a
Andrew Smith <diver.vlz@gmail.com>
2022-03-01 23:32:10 +0300

    CamelCaseJSONRenderer test - OK

--- service/settings.py
+++ service/settings.py
@@ -139,3 +139,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #         'rest_framework_xml.renderers.XMLRenderer',
 #     ],
 # }
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_RENDERER_CLASSES': (
+        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    ),
+
+    'DEFAULT_PARSER_CLASSES': (
+        'djangorestframework_camel_case.parser.CamelCaseFormParser',
+        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    ),
+}
