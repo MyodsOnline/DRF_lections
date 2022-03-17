2f959be4d7a164fa56bebcf83bb02f32abb99a9f
Andrew Smith <diver.vlz@gmail.com>
2022-03-17 12:57:56 +0300

    first_part

--- authors/views.py
+++ authors/views.py
@@ -1,8 +1,23 @@
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Author, Biography, Book, Article
-from .serializers import AuthorModelSerializer, BiographyModelSerializer, BookModelSerializer, ArticleModelSerializer
+from .serializers import AuthorModelSerializer, BiographyModelSerializer, BookModelSerializer, ArticleModelSerializer, SimpleAuthorModelSerializer
+
+
+class AuthorApiView(APIView):
+
+    def get(self, request):
+        # print(request.query_params)
+        # print(request.headers)
+        authors = Author.objects.all()
+        serializer = SimpleAuthorModelSerializer(authors, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        return Response('tatata - post')
 
 
 class AuthorModelViewSet(ModelViewSet):
--- service/urls.py
+++ service/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from authors.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet
+from authors.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet, AuthorApiView
 
 router = DefaultRouter()
 router.register('authors', AuthorModelViewSet)
@@ -29,4 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('simpa/', AuthorApiView.as_view()),
 ]
