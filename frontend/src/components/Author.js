241ab6f4bfd3578970656847078dc6085d3cc9d3
Andrew Smith <diver.vlz@gmail.com>
2022-03-22 12:08:14 +0300

    webinar_5 init commit

--- frontend/src/components/Author.js
+++ frontend/src/components/Author.js
@@ -4,15 +4,9 @@ import React from 'react'
 const AuthorItem = ({author}) => {
    return (
        <tr class="table_tr">
-           <td>
-               {author.first_name}
-           </td>
-           <td>
-               {author.last_name}
-           </td>
-           <td>
-               {author.birthday_year}
-           </td>
+           <td>{author.firstName}</td>
+           <td>{author.lastName}</td>
+           <td>{author.birthdayYear}</td>
        </tr>
    )
 }
@@ -20,15 +14,11 @@ const AuthorItem = ({author}) => {
 const AuthorList = ({authors}) => {
    return (
        <table>
-           <th>
-               First name
-           </th>
-           <th>
-               Last Name
-           </th>
-           <th>
-               Birthday year
-           </th>
+           <tr>
+               <th>First name</th>
+               <th>Last Name</th>
+               <th>Birthday year</th>
+           </tr>
            {authors.map((author) => <AuthorItem author={author} />)}
        </table>
    )
