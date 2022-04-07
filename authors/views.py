760a4b119fccffdc6cd171d91a2fab0cc09d0710
Andrew Smith <diver.vlz@gmail.com>
2022-04-07 13:18:51 +0300

    URLPathVersioning

--- authors/views.py
+++ authors/views.py
@@ -12,65 +12,22 @@ from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 from .filters import ArticleFilter
 
 
-# class AuthorApiView(APIView):
-#     # def get(self, request):
-#     #     # print(request.query_params)
-#     #     # print(request.headers)
-#     #     authors = Author.objects.all()
-#     #     serializer = SimpleAuthorModelSerializer(authors, many=True)
-#     #     return Response(serializer.data)
-#     #
-#     # def post(self, request):
-#     #     return Response('tatata - post')
-
-# class AuthorApiView(ListAPIView, CreateAPIView):
-#     renderer_classes = [JSONRenderer]
-#     queryset = Author.objects.all()
-#     serializer_class = AuthorModelSerializer
-#
-#
-# class AuthorApiView(viewsets.ViewSet):
-#     def list(self, request):
-#         authors = Author.objects.all()
-#         serializer = SimpleAuthorModelSerializer(authors, many=True)
-#         return Response(serializer.data)
-#
-#
-# class AuthorApiView(viewsets.ModelViewSet):
-#     queryset = Author.objects.all()
-#     serializer_class = AuthorModelSerializer
-#
-#     def list(self, request, *args, **kwargs):
-#         print('Changed list in ModelViewSet')
-#         return super().list(request, *args, **kwargs)
-#
-#
-# class AuthorApiView(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-#     queryset = Author.objects.all()
-#     serializer_class = AuthorModelSerializer
-
-
-class AuthorModelViewSet(ModelViewSet):
+class AuthorModelViewSet(ListAPIView):
     permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
     filterset_fields = ['first_name', 'birthday_year']
 
-    # def get_queryset(self):
-    #     param = self.request.headers.get('param')
-    #     return Author.objects.filter(birthday_year__contains=param)
+    def get_serializer_class(self):
+        if self.request.version == '1':
+            return SimpleAuthorModelSerializer
+        return AuthorModelSerializer
 
 
 class BiographyModelViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]
     queryset = Biography.objects.all()
     serializer_class = BiographyModelSerializer
-#
-#
-# class BiographyApiView(RetrieveAPIView):
-#     renderer_classes = [JSONRenderer]
-#     queryset = Biography.objects.all()
-#     serializer_class = BiographyModelSerializer
 
 
 class BookModelViewSet(ModelViewSet):
--- service/urls.py
+++ service/urls.py
@@ -1,20 +1,5 @@
-"""service URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -22,7 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from authors.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet
 
 router = DefaultRouter()
-router.register('authors', AuthorModelViewSet)
+# router.register('authors', AuthorModelViewSet)
 router.register('books', BookModelViewSet)
 router.register('articles', ArticleModelViewSet)
 router.register('biographies', BiographyModelViewSet)
@@ -32,6 +17,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
     path('api-token-auth/', views.obtain_auth_token),
-    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    re_path(r'^api/(?P<version>\d)/authors/$', AuthorModelViewSet.as_view()),
 ]
